044ba6cc097dc46ebfaa34e3f40e4745d211c852
unknown <HossamAlsamman@Hossamit.mw.local>
2025-09-08 11:00:47 +0300

    morse code converter

---
+++ morse-code-converter.py
@@ -0,0 +1,21 @@
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..',
+    'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.',
+    ' ': '/'  # Use slash for space between words
+}
+message = input("Enter your message: ")
+message = message.upper()
+encrypted_message = [
+    MORSE_CODE_DICT.get(letter, letter)  # Use '?' for unknown characters
+    for letter in message
+]
+
+print(f"Your encrypted message is: {"".join(encrypted_message)}")
